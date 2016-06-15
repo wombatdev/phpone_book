3c12cd5ffa91e62b7cdd0c6572fc5bfba35540c8
James <james@team-wombat.com>
2016-06-15 09:25:03 -0400

    Created boilerplate HTML and used Robin's CSS

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHPWNAGE BOOK</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <!-- <script src="war.js"></script> -->
+</head>
+<body>
+
+<?php
+
+echo "test";
+
+ ?>
+
+</body>
+</html>
