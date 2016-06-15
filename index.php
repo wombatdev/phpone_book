4fb1908e5d54fe0d62c954ec4a9d844882101273
James <james@team-wombat.com>
2016-06-15 11:13:33 -0400

    Sort of have HTML contact display working

--- index.php
+++ index.php
@@ -10,7 +10,43 @@
 <body>
 
 <h1>PHPWNED BOOK</h1>
-<!-- <h2>Enter your name for PHPWNAGE:</h2> -->
+
+<?php
+
+if (file_exists("entries.json")) {
+    $str=file_get_contents("entries.json");
+    $json = json_decode($str, true);
+    for ($i=0; $i<sizeof($json); $i++) {
+        echo "<h2>";
+        print_r($json[$i]['name']);
+        echo "</h2>";
+        echo "<p>";
+        print_r($json[$i]['number']);
+        echo "</p>";
+        echo "
+            <form action='#' method='POST'>
+                <input type='hidden' name='do_what' value='delete' />
+                <input type='hidden' name='entry[name]' value=' ted' />
+                <button type='submit'>Delete</button>
+            </form>
+            ";
+    };
+    $entry = (object) array(
+        'name' => $_POST["name"],
+        'number' => $_POST["number"]
+    );
+    array_push($json, $entry);
+    $encode = json_encode($json, true);
+    file_put_contents("entries.json",$encode);
+
+}
+else {
+    echo "Whiskey niner";
+}
+
+
+
+?>
 
 <form method="post" action="index.php">
     <fieldset>
@@ -26,6 +62,26 @@
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your number is: <?php echo $_POST["number"]; ?>
+
+
+
+
+<script>
+
+// $.ajax({
+//     url:'entries.json',
+//     type:'HEAD',
+//     error: function() {
+//         console.log("Nope");
+//     },
+//     success: function() {
+//         console.log("Yep!");
+//
+//     }
+// });
+
+
+</script>
 
 </body>
 </html>
